2b3028ed9d9f5108e7b4f3c86894a705a263954d
Lakshmi Narayana MG <146064374+LakshmiNarayana2003@users.noreply.github.com>
2024-07-27 20:39:57 +0530

    Update cust_hume.py

--- cust_hume.py
+++ cust_hume.py
@@ -4,7 +4,7 @@ from hume import HumeVoiceClient, MicrophoneInterface
 
 
 async def main() -> None:
-    client = HumeVoiceClient("51kDwMHG87GnykkoAhVMdXxeLUrtyyctUQfcb6Gmo9YqArwG")
+    client = HumeVoiceClient("KEY")
 
     async with client.connect(config_id="620afc8d-1826-4869-9aff-7eda8ad68a16") as socket:
         await MicrophoneInterface.start(socket)
